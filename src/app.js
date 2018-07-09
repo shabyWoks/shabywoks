9bf2eb28a9080c3432b8e3e264906082307f9086
shabyWoks <shaby1@outlook.com>
2018-07-09 12:46:14 +0530

    Inital Commit

---
+++ src/app.js
@@ -0,0 +1,4 @@
+console.log("app.js is running");
+const jsx = (<p>Testing</p>);
+
+ReactDOM.render(jsx, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public', 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            { 
+                test: /\.js/, 
+                use: 'babel-loader', 
+                exclude: /node_modules/
+            }
+        ]
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        publicPath: '/dist/'
+    }
+}
